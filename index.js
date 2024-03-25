70792ed8b37e5d0a43c4db35b8815e8e9720eab1
Adeeba-Khan <98907334+Adeeba-Khan@users.noreply.github.com>
2024-03-25 12:20:27 +0530

    Second branch commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Write your code below:
+const heading = document.getElementById('main-heading');
+heading.textContent='Fruit World';
+heading.style.color='orange';
+const header = document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom = '3px solid orange';
+const heading2 = document.getElementById('basket-heading');
+heading2.style.color='green';
+const thank = document.getElementById('thanks');
+thank.innerHTML="<p>Please visit us again</p>"
