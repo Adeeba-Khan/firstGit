4d371dda88a18fbaca9293daf4af17e1d1c2ff18
Adeeba-Khan <98907334+Adeeba-Khan@users.noreply.github.com>
2024-03-25 16:02:47 +0530

    fifth commit

--- index.js
+++ index.js
@@ -23,3 +23,45 @@ lists[4].style.color='blue';
 for(let i = 0; i<lists.length; i++){
   lists[i].style.fontStyle='italic';
 }
+// Write the code as shown in the video below:
+// QuerySelector is used to grab only single element
+const mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'center';
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+fruits.style.padding = '30px';
+fruits.style.margin = '30px';
+fruits.style.width = '50%';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType = 'none';
+
+const basketHeading = document.querySelector('h2');
+basketHeading.style.marginLeft = '30px';
+
+// QuerySelectorAll is used to grab all the elements with same class/tag
+const fruitItems = document.querySelectorAll('.fruit');
+
+for(let i=0; i<fruitItems.length; i++){
+fruitItems[i].style.backgroundColor = 'white';
+fruitItems[i].style.padding = '10px';
+fruitItems[i].style.margin = '10px';
+fruitItems[i].style.borderRadius = '5px';
+
+}
+
+
+
+const OddFruitItems = document.querySelectorAll('.fruit:nth-child(odd)');
+for(let i=0; i<OddFruitItems.length; i++){
+OddFruitItems[i].style.backgroundColor = 'lightgray';
+}
+// Write answer to the questions asked below:
+const basketHead = document.querySelector('#basket-heading');
+
+basketHead.style.color = 'brown';
+const EvenFruitItems = document.querySelectorAll('.fruit:nth-child(even)');
+for (let i = 0; i < EvenFruitItems.length; i++) {
+    EvenFruitItems[i].style.backgroundColor = 'brown'; // Corrected to set background color to "brown"
+    EvenFruitItems[i].style.color = 'white';
+}
